552c15b8c335872641b79ef08df9da763d2c8256
Kevin McKinnerney <40665896+kemckinnmsft@users.noreply.github.com>
2019-01-23 19:21:59 -0600

    update scripts

--- Scripts/GenerateAuthToken.ps1
+++ Scripts/GenerateAuthToken.ps1
@@ -6,8 +6,8 @@ $WebAppKey = New-Guid
 $Date = Get-Date
 New-AzureADApplicationPasswordCredential -ObjectId $WebApp.ObjectID -startDate $Date -endDate $Date.AddYears(1) -Value $WebAppKey.Guid -CustomKeyIdentifier "AIPClient"
 
-$AIPServicePrincipal = Get-AzureADServicePrincipal -All $true | ? {$_.DisplayName -eq 'AIPOnBehalfOf'}
-$AIPPermissions = $AIPServicePrincipal | select -expand Oauth2Permissions
+$AIPServicePrincipal = Get-AzureADServicePrincipal -All $true | Where-Object {$_.DisplayName -eq 'AIPOnBehalfOf'}
+$AIPPermissions = $AIPServicePrincipal | Select-Object -expand Oauth2Permissions
 $Scope = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $AIPPermissions.Id,"Scope"
 $Access = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
 $Access.ResourceAppId = $WebApp.AppId
@@ -18,4 +18,4 @@ $NativeApp = Get-AzureADApplication -Filter "DisplayName eq 'AIPClient'"
 New-AzureADServicePrincipal -AppId $NativeApp.AppId
 
 "Set-AIPAuthentication -WebAppID " + $WebApp.AppId + " -WebAppKey " + $WebAppKey.Guid + " -NativeAppID " + $NativeApp.AppId | Out-File ~\Desktop\Set-AIPAuthentication.txt
-Start ~\Desktop\Set-AIPAuthentication.txt
+Start-Process ~\Desktop\Set-AIPAuthentication.txt
