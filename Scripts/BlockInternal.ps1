6f95cdd147384d4dea318b348636e2c74b701b44
Kevin McKinnerney <40665896+kemckinnmsft@users.noreply.github.com>
2019-01-24 00:19:46 -0600

    exscripts

---
+++ Scripts/BlockInternal.ps1
@@ -0,0 +1,8 @@
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+Add-Type -AssemblyName Microsoft.VisualBasic
+$labelid = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the LabelId for your All Employees Label', 'LabelId')
+$labeltext = "MSIP_Label_"+$labelid+"_enabled=true"
+New-TransportRule -name "Block Confidential Contoso All Employees" -SentToScope notinorganization -HeaderContainsMessageHeader  "msip_labels" -HeaderContainsWord $labeltext -RejectMessageReasonText “Contoso internal messages cannot be sent to external recipients.”
