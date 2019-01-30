f4684311efd8dfd92c428a8ace013dafac44daca
Kevin McKinnerney <40665896+kemckinnmsft@users.noreply.github.com>
2019-01-30 09:05:57 -0500

    Update BlockInternal.ps1

--- Scripts/BlockInternal.ps1
+++ Scripts/BlockInternal.ps1
@@ -7,4 +7,4 @@ If(Get-TransportRule *Delete*){Remove-TransportRule *Delete*}
 Add-Type -AssemblyName Microsoft.VisualBasic
 $labelid = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the LabelId for your All Employees Label', 'LabelId')
 $labeltext = "MSIP_Label_"+$labelid+"_enabled=true"
-New-TransportRule -name "Block Confidential Contoso All Employees" -SentToScope notinorganization -HeaderContainsMessageHeader  "msip_labels" -HeaderContainsWord $labeltext -RejectMessageReasonText “Contoso internal messages cannot be sent to external recipients.”
+New-TransportRule -name "Block Confidential Contoso All Employees" -SentToScope notinorganization -HeaderContainsMessageHeader "msip_labels" -HeaderContainsWord $labeltext -RejectMessageReasonText “Contoso internal messages cannot be sent to external recipients.”
